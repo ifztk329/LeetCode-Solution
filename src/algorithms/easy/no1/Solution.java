82d351e2717f348012f4c16ba95740a04aa6c61e
黎炘 <tingkai.ztk@antfin.com>
2020-01-26 16:54:45 +0800

    add Two-pass Hash Table Solution

--- src/algorithms/easy/no1/Solution.java
+++ src/algorithms/easy/no1/Solution.java
@@ -1,5 +1,8 @@
 package algorithms.easy.no1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * https://leetcode.com/problems/two-sum/submissions/
@@ -12,21 +15,55 @@ package algorithms.easy.no1;
  * return [0, 1].
  *
  *
- * Brute Force
  *
  */
 class BruteForce {
+    /**
+     * 57 ms
+     * 39.6 MB
+     * @param nums
+     * @param target
+     * @return
+     */
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target)
-                {
-                    return new int[]{i,j};
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
                 }
             }
         }
 
         return null;
 
+    }
+}
+
+/**
+ * Two-pass Hash Table
+ */
+class TwoPassHashTable {
+    /**
+     * 2 ms
+     * 41.8 MB	
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] {i, map.get(complement)};
+            }
+        }
+
+        return null;
     }
 }
